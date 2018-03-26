236faa3e69966149ef48f6d10832f16a61694475
Kusuma Ruslan <wangsamax@gmail.com>
2018-03-26 11:13:03 +0700

    Add files via upload

---
+++ my_preference/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "my_preference",
+
+    'summary': """
+        Edit my preference module""",
+
+    'description': """
+        This module is to display and edit more user information in preference menu
+    """,
+
+    'author': "Kusuma Ruslan",
+    'website': "http://www.wangsamas.com",
+
+    'category': 'Extra Tools',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'views/views.xml',
+    ],
+}
